b9169fb53aef431e4bdb8118d4e15e82f161b17a
kevinmao <maojunhui@yahoo.com>
2015-11-04 20:58:14 -0800

    add yelp_academic_dataset

---
+++ config.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## Where am I
+export WHERE_AM_I="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+############################################
+### global options subject to change
+############################################
+
+## Project home
+export PROJECT_HOME=${WHERE_AM_I}
+
+## Mallet home
+export MALLET_HOME=${PROJECT_HOME}/Mallet
+
+############################################
+### corpus processing 
+############################################
+## corpus data 
+export CORPUS_DATA=${PROJECT_HOME}/yelp_data
+
+## yelp data zipped
+export YELP_DATA_ZIP=${CORPUS_DATA}/raw/zip
+
+## yelp data json
+export YELP_DATA_JSON=${CORPUS_DATA}/raw/json
+
+## processed data in tsv format
+export YELP_DATA_TSV=${CORPUS_DATA}/processed/tsv
+
+## training data
+export TRAIN_DATA=${CORPUS_DATA}/processed/train
+
+## test data
+export TEST_DATA=${CORPUS_DATA}/processed/test
+
+############################################
+### training and testing
+############################################
+## Mallet data
+export MALLET_DATA=${PROJECT_HOME}/mallet-data
+
+############################################
+### report and visualization
+############################################
+export REPORT_DATA=${PROJECT_HOME}/report
+
+############################################
+### functions
+############################################
+# logger
+function LOGGER {
+    printf "%s %s\n" "[$(date +"%Y-%m-%d %H:%M:%S")]" "$@"
+}
+
---
+++ docprocess/xunzip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# global vars
+source ../config.sh
+
+# reset
+rm -fr ${YELP_DATA_JSON}
+mkdir -p ${YELP_DATA_JSON}
+
+prefix="yelp_dataset_challenge_academic_dataset"
+tar xvfz ${YELP_DATA_ZIP}/${prefix}.tgz -C ${YELP_DATA_JSON}
+mv ${YELP_DATA_JSON}/${prefix}/* ${YELP_DATA_JSON}
+rm -fr ${YELP_DATA_JSON}/${prefix}
+rm -f ${YELP_DATA_JSON}/*pdf
+
