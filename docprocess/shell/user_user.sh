16b3b503351836ef8ca77d0a0681630365cc80a8
kevinmao <maojunhui@yahoo.com>
2015-11-09 09:32:52 -0800

    fixed user-user

--- docprocess/shell/user_user.sh
+++ docprocess/shell/user_user.sh
@@ -10,7 +10,7 @@ prefix=yelp_academic_dataset
 ftype=user
 LOGGER "Processing ${prefix}_${ftype}.json"
 rm -f ${YELP_DATA_TSV}/user_user_friend_edges.tsv
-python ${DOC_PROCESS}/user_user.py \
+python ${DOC_PROCESS_PYTHON}/user_user.py \
 --user ${YELP_DATA_JSON}/${prefix}_${ftype}.json \
 --user_keys ${YELP_DATA_TSV}/${ftype}_keys.tsv \
 --user_user_friend_edges ${YELP_DATA_TSV}/user_user_friend_edges.tsv
