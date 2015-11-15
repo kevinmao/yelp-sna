934a2090ada3b0625e2f7dad1d5c29696a9edccc
kevinmao <maojunhui@yahoo.com>
2015-11-15 14:33:56 -0800

    renamed

--- analysis/python/graph_info.py
+++ analysis/python/graph_info.py
@@ -1,13 +1,30 @@
-#################################  
-## CS224W hw1q1
-#################################  
-
 import snap
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 import plfit
 import argparse
 from operator import itemgetter
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def load_graph(inputFile, outputFile):
+    User = set()
+    Business = set()
+    Edge = set()
+    with open(inputFile) as fin:
+        for line in fin:
+            if line.find('# user_id') >= 0: continue
+            row = line.strip('\n').split('\t')
+            uid = int(row[0])
+            bid = int(row[1])
+            e = (uid, bid)
+            User.add(uid)
+            Business.add(bid)
+            Edge.add(e)
+    with open(outputFile, 'a') as fout:
+        fout.write("\nGraph Info: \n")
+        fout.write("\tNumber of Users: " + str(len(User)) + "\n")
+        fout.write("\tNumber of Businesses: " + str(len(Business)) + "\n")
+        fout.write("\tNumber of Reviews: " + str(len(Edge)) + "\n")
+    return User, Business, Edge
 
 def getDegreeToCount(G):
     """ get (degree, count-of-node, fraction-of-node) sequences
@@ -62,23 +79,29 @@ def plfitDegreeDistr(G, outputFile):
         fout.write("\nplfit fitted parameters:\n")
         fout.write("\txmin = " + str(fit._xmin) + "\n")
         fout.write("\talpha = " + str(fit._alpha) + "\n")
-        fout.write("\tks = " + str(fit._ks) + "\n")
-        fout.write("\tks_prob = " + str(fit._ks_prob) + "\n")
-        fout.write("\tlikelihood = " + str(fit._likelihood) + "\n")
 
-def topWCC(G, outputFile, n=10):
+def clustCf(G, outputFile):
+    cf = snap.GetClustCf(G)
+    with open(outputFile, 'a') as fout:
+        fout.write("\nclustering coefficient:\n")
+        fout.write("\tcf = " + str(cf) + "\n")
+    
+def topWCC(G, outputFile, n=5):
     WccSzCnt = snap.TIntPrV()
     snap.GetWccSzCnt(G, WccSzCnt)
+    Cnt = WccSzCnt.Len()
     L = []
-    for i in range (0, WccSzCnt.Len()):
+    for i in range (Cnt):
         e = (WccSzCnt[i].Val1.Val, WccSzCnt[i].Val2.Val)
         L.append(e)
+
     # sort and get top 10    
     L = sorted(L, key=itemgetter(0), reverse=True)
-
     with open(outputFile, 'a') as fout:
         fout.write("\nWCC Info: \n")
-        for i in range(n):
+        fout.write("\tWccSzCnt.Len(): " + str(Cnt) + "\n")
+        m = min(n, Cnt)
+        for i in range(m):
             e = L[i]
             fout.write("\tWccSzCnt[" + str(i) + "] = (" + str(e[0]) + "," + str(e[1]) + ")\n")
     
@@ -98,6 +121,9 @@ def main(args):
 
     # plift
     plfitDegreeDistr(G, degree_dist_info_file)
+    
+    # clustering coefficient
+    clustCf(G, degree_dist_info_file)
     
     # wcc
     topWCC(G, degree_dist_info_file)
--- analysis/shell/degree_dist.sh
+++ analysis/shell/degree_dist.sh
@@ -6,14 +6,27 @@ mkdir -p ${TRAIN_DATA}
 
 LOGGER "START..."
 
+review_all=${TRAIN_DATA}/ub_review_all_edges.tsv
 degree_dist_info=${YELP_DATA_STATS}/degree_dist_info.txt
 degree_dist_plot=${YELP_DATA_STATS}/degree_dist_plot.pdf
-rm -f ${degree_dist_info} ${degree_dist_plot}
 
+review_core=${TRAIN_DATA}/ub_review_train_core_edges.tsv
+degree_dist_traincore_info=${YELP_DATA_STATS}/degree_dist_traincore_info.txt
+degree_dist_traincore_plot=${YELP_DATA_STATS}/degree_dist_traincore_plot.pdf
+
+LOGGER "Processing ${review_all}"
+rm -f ${degree_dist_info} ${degree_dist_plot}
 python ${STATS_PYTHON}/degree_dist.py \
---ub_review_edges ${TRAIN_DATA}/ub_review_all_edges.tsv \
+--ub_review_edges ${review_all} \
 --degree_dist_info ${degree_dist_info} \
 --degree_dist_plot ${degree_dist_plot} 
+
+LOGGER "Processing ${review_core}"
+rm -f ${degree_dist_traincore_info} ${degree_dist_traincore_plot}
+python ${STATS_PYTHON}/degree_dist.py \
+--ub_review_edges ${review_core} \
+--degree_dist_info ${degree_dist_traincore_info} \
+--degree_dist_plot ${degree_dist_traincore_plot} 
 
 LOGGER "Done."
 
