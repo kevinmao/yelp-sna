722f2e51b3af5eab2e286fc691f39095b518b2cc
kevinmao <maojunhui@yahoo.com>
2015-11-15 16:47:59 -0800

    check overall recall

---
+++ analysis/shell/check_recall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# global vars
+source ../../config.sh 
+mkdir -p ${YELP_DATA_STATS}
+
+LOGGER "START..."
+
+ub_similarity=${YELP_DATA_TRAIN}/ub_similarity.tsv
+test_core=${YELP_DATA_TRAIN}/ub_review_test_core_edges.tsv
+stats=${YELP_DATA_STATS}/stats.tsv
+
+{
+echo
+echo "------------ recall for ub_similarity ------------"
+python ${STATS_PYTHON}/check_common_edges.py \
+--train_data ${ub_similarity} \
+--test_data ${test_core}
+} >> ${stats}
+
+LOGGER "Done."
+
